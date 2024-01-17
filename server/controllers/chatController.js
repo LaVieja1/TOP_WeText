3cc13af0b011b2c79f054fb2624b33c3971e4417
santicano <canosantiago404@gmail.com>
2024-01-17 19:24:39 -0300

    añadido chat y message controller

---
+++ server/controllers/chatController.js
@@ -0,0 +1,53 @@
+const Chat = require('../models/chat');
+const Message = require('../models/message');
+const User = require('../models/user');
+
+exports.accessChat = async (req, res) => {
+    const friendId = req.body.friendId;
+
+    if (!friendId || friendId === req.userId) {
+        return res.json(false);
+    }
+
+    let isChat = await Chat.find({
+        isGroup: false,
+        $and: [
+            { users: { $elemMatch: { $eq: req.userId } } },
+            { users: { $elemMatch: { $eq: friendId } } },
+        ]
+    })
+    .populate("users", "-password")
+    .populate("lastMsg")
+
+    isChat = await User.populate(isChat, {
+        path: 'lastMsg.sender',
+        select: 'name pictureUrl email'
+    });
+
+    if (isChat.length > 0) {
+        return res.json(isChat[0]);
+    }
+
+    //If chat don't exist, create one
+    const chatData = {
+        chatName: "sender",
+        isGroupChat: false,
+        users: [req.userId, friendId]
+    }
+
+    try {
+        const createdChat = await Chat.create(chatData);
+        const fullChat = await Chat.findOne({ _id: createdChat._id }).populate('users', '-password');
+        return res.json(fullChat);
+    } catch (error) {
+        return res.json(error);
+    }
+}
+
+exports.findUserChats = async (req, res) => {
+    const chats = await Chat.find({ users: { $elemMatch: { $eq: req.userId } } })
+        .populate("users", "-password").populate('lastMsg', '-chat -_id -createdAt').sort({ updatedAt: -1 });
+    const chatsId = chats.map(chat => chat._id);
+    const unseenMessages = await Message.find({ isSeen: false, chat: { $in: chatsId } }).select('_id chat sender');
+    return res.json({ chats, unseenMessages });
+}
