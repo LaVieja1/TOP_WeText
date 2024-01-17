83afb6581c5d3b847c6669f88777885e3fbaed0d
santicano <canosantiago404@gmail.com>
2024-01-17 17:52:37 -0300

    configurado mongoDB, falta conectar

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+// CORS
+app.use(cors({
+  origin: 'http://localhost:5173/',
+  credentials: true,
+}));
+
+// DB
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+console.log('Conectado a mongoDB');
+db.on('error', console.error.bind((console, 'MongoDb error de conexión')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
